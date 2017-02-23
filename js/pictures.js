44e5606417ac53025784ee0f93b030a24bb6c303
Valeria Kasatkina <orionella@gmail.com>
2017-02-23 10:16:50 -0800

    Sort Pictures

--- js/pictures.js
+++ js/pictures.js
@@ -6,11 +6,13 @@ window.addInfoToPicture = function (image, likes, comments, data) {
   comments.innerText = data.comments.length;
 };
 
+
 (function () {
   var DATA_URL = ' https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
 
   window.load(DATA_URL, function (response) {
     window.pictures = response;
+
     var template = document.querySelector('#picture-template');
     var pictureToClone = template.content.querySelector('.picture');
     var picturesContainer = document.querySelector('.pictures');
@@ -32,5 +34,63 @@ window.addInfoToPicture = function (image, likes, comments, data) {
 
     window.pictures.forEach(renderPictureElement);
 
+
+    var pictureFilters = document.querySelector('.filters');
+    var popularPictures = document.querySelector('#filter-popular');
+    var newPictures = document.querySelector('#filter-new');
+    var discussedPictures = document.querySelector('#filter-discussed');
+
+    var removeAllPictures = function (parent) {
+      var i = parent.childElementCount;
+      while (i !== 0) {
+        parent.removeChild(parent.childNodes[i]);
+        i--;
+      }
+    };
+
+    var getRandomElementExcept = function (array, arraynew) {
+      var isElementNew = false;
+      var randomElement;
+
+      while (!isElementNew) {
+        randomElement = window.utils.getRandomElement(array);
+        isElementNew = true;
+        arraynew.forEach(function (item) {
+          if (randomElement === item) {
+            isElementNew = false;
+          }
+        });
+      }
+      return randomElement;
+    };
+
+
+    pictureFilters.classList.remove('hidden');
+
+    popularPictures.addEventListener('click', function () {
+      removeAllPictures(picturesContainer);
+      window.pictures.forEach(renderPictureElement);
+    });
+
+    newPictures.addEventListener('click', function () {
+      var newPicturesArray = [];
+      var i = 0;
+      while (i < 10) {
+        newPicturesArray[newPicturesArray.length] = getRandomElementExcept(window.pictures, newPicturesArray);
+        i++;
+      }
+      removeAllPictures(picturesContainer);
+      newPicturesArray.forEach(renderPictureElement);
+    });
+
+    discussedPictures.addEventListener('click', function () {
+      removeAllPictures(picturesContainer);
+      var newPicturesArray = window.pictures.slice(window.pictures);
+      newPicturesArray.sort(function (item1, item2) {
+        return item2.comments.length - item1.comments.length;
+      });
+      newPicturesArray.forEach(renderPictureElement);
+    });
   });
 })();
+
--- js/utils.js
+++ js/utils.js
@@ -4,12 +4,19 @@ window.utils = (function () {
   var ENTER_KEY_CODE = 13;
   var ESC_KEY_CODE = 27;
 
+  var randomNumber = function (maxValue) {
+    return Math.floor(Math.random() * maxValue);
+  };
+
   return {
     isEnterKey: function (event) {
       return (event.keyCode === ENTER_KEY_CODE);
     },
     isEscapeKey: function (event) {
       return (event.keyCode === ESC_KEY_CODE);
+    },
+    getRandomElement: function (array) {
+      return array[randomNumber(array.length)];
     }
   };
 })();
